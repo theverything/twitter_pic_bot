0bf1b7f4374016f58207dccbf9c97b01cb268691
theverything <j3ffhorn@gmail.com>
2013-03-16 09:51:54 -0700

    add logger

--- post.rb
+++ post.rb
@@ -5,17 +5,23 @@ def post_cycle
   begin
     reddit = Reddit.new("earthporn", 1)
     tweet = Post.new(reddit)
-    puts Time.now
     tweet.to_twitter
-    puts tweet.the_post
+    logger tweet.the_post
     sleep 3600
     post_cycle
   rescue
-    puts "Duplicate... I'll try again in an hour"
+    logger "Duplicate... I'll try again in an hour"
     sleep 3600
     post_cycle
   end
   
+end
+
+def logger(message)
+  File.open("./log.txt", 'a+') do |file|
+    file.write("#{Time.now}\n")
+    file.write("#{message}\n")
+  end
 end
 
 post_cycle
