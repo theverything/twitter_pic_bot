5b70535e3df46ab5a92b4bb513d6e80412e7a038
theverything <j3ffhorn@gmail.com>
2013-03-16 11:26:29 -0700

    move into loop block

--- post.rb
+++ post.rb
@@ -1,29 +1,20 @@
 require './reddit.rb'
 require './twitter.rb'
+require './logger.rb'
 
 def post_cycle
   begin
     reddit = Reddit.new("earthporn", 1)
     tweet = Post.new(reddit)
     tweet.to_twitter
-    logger tweet.the_post
-    sleep 3600
-    post_cycle
+    Logger.write tweet.the_post
+    sleep 900
   rescue
-    logger "Duplicate... I'll try again in an hour"
-    sleep 3600
-    post_cycle
+    Logger.write "Duplicate... I'll try again in 15 mins"
+    sleep 900
   end
-  
+
+
+loop do
+  post_cycle
 end
-
-def logger(message)
-  File.open("./log.txt", 'a+') do |file|
-    file.write("#{Time.now}\n")
-    file.write("#{message}\n")
-  end
-end
-
-post_cycle
-
-
